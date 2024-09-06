525ea9978b80f9eaf6789ad1382db01a90777714
Izabelle Alves <izabelle.alvesbl@gmail.com>
2024-09-06 15:44:37 -0300

    Atualizações feitas na interação da página

--- src/teste.ts
+++ src/teste.ts
@@ -9,8 +9,11 @@ class Post {
   private _description: string; // atributo privado
   private _createdAt: Date = new Date(); // atributo privado
   private _numberOfLikes: number = 0; // atributo privado
+  private _hashtag: string;
   private _id: string = randomUUID();
   private _isLiked: boolean = false;
+  private _isFollowed: boolean = false;
+  private _isSaved: boolean = false;
   private _avatarUrl: string;
   private _imageUrl: string;
 
@@ -19,13 +22,14 @@ class Post {
     userName: string,
     avatarUrl: string,
     imageUrl: string,
-    description: string
+    description: string,
+    hashtag: string
   ) {
     this._userName = userName; // inicialização da propriedade userName
     this._description = description; // inicialização da propriedade titulo
     this._avatarUrl = avatarUrl;
     this._imageUrl = imageUrl; // inicialização do número de curtidas com 0
-    this._isLiked;
+    this._hashtag = hashtag;
   }
 
   render() {
@@ -40,13 +44,13 @@ class Post {
               <div class="avatar">
                 <img
                   src="${this._avatarUrl}"
-                  alt="${this._userName}"
+                  alt=""
                   srcset=""
                 />
               </div>
               <span>${this._userName}</span>
             </div>
-            <div id="btn-follow" class="follow-options" onclick="follow()">
+            <div id="btn-follow-${this._id}" class="follow-options">
               <div class="follow">Follow</div>
               <div>...</div>
             </div>`;
@@ -63,7 +67,7 @@ class Post {
     const postIcons = document.createElement("div");
     postIcons.className = "post-icons";
     postIcons.innerHTML = `<div>
-              <div id="btn-like" onclick="like()">
+              <div id="btn-like-${this._id}">
                 <i class="fa fa-heart-o"></i>
               </div>
 
@@ -75,20 +79,20 @@ class Post {
                 <i class="fa fa-send-o"></i>
               </div>
             </div>
-            <div id="btn-save" onclick="save()">
+            <div id="btn-save-${this._id}">
               <i class="fa fa-bookmark-o"></i>
             </div>`;
 
     const postLike = document.createElement("div");
     postLike.className = "post-likes";
     postLike.innerHTML = ` <i class="fa fa-heart"></i>
-            <span id="like-count">0</span> likes`;
+            <span id="like-count-${this._id}">0</span> likes`;
 
     const postDescription = document.createElement("div");
     postDescription.className = "post-description";
     postDescription.innerHTML = `<div class="user_name">username</div>
             <div class="description">
-              your title here <span class="hash-tag">#hashtag</span>
+              ${this._description} <span class="hash-tag">${this._hashtag}</span>
             </div>`;
 
     postContainer.append(
@@ -98,21 +102,72 @@ class Post {
       postLike,
       postDescription
     );
-    document.body.appendChild(postContainer);
 
+    const mainContainer = document.getElementById("container-position");
+    if (mainContainer) {
+      mainContainer.appendChild(postContainer);
+    }
+
+    const likeButton = document.querySelector(`#btn-like-${this._id}`)
+    if (likeButton){
+      likeButton.addEventListener("click", () => this.like());
+    }
+
+    const saveButton = document.querySelector(`#btn-save-${this._id}`);
+    if (saveButton) {
+      saveButton.addEventListener("click", () => this.save());
+    }
+
+    const followButton = document.querySelector(`#btn-follow-${this._id}`);
+    if (followButton) {
+      followButton.addEventListener("click", () => this.follow());
+    }
+    
     return postContainer;
   }
 
   like() {
-    this._isLiked = !this._isLiked;
 
-    // incrementa o número de likes
-    if (this._isLiked) {
-      this._numberOfLikes += 1;
-    } else {
-      // decrementa o número de likes
-      this._numberOfLikes -= 1;
-    }
+    const button = document.getElementById(`btn-like-${this._id}`);
+    const icon = button?.querySelector("i");
+
+    let likeCountElement = document.getElementById(`like-count-${this._id}`);
+
+    if (!icon || !likeCountElement) return;
+
+    icon.classList.toggle("fa-heart");
+    icon.classList.toggle("liked");
+    icon.classList.toggle("fa-heart-o");
+
+    this._numberOfLikes += this._isLiked ? -1 : 1;
+    likeCountElement.innerText = this._numberOfLikes.toString();
+
+    this._isLiked = !this._isLiked;
+  }
+
+  save() {
+    const button = document.getElementById(`btn-save-${this._id}`);
+    const icon = button?.querySelector("i");
+
+    if (!icon) return;
+
+    icon.classList.toggle("fa-bookmark");
+    icon.classList.toggle("saved");
+    icon.classList.toggle("fa-bookmark-o");
+
+    this._isSaved = !this._isSaved;
+  }
+
+  follow() {
+    const button = document.querySelector(`#btn-follow-${this._id}`);
+    const icon = button?.querySelector("div");
+
+    if (!icon) return;
+
+    icon.classList.toggle("followed-style");
+    icon.innerHTML = this._isFollowed ? "Follow" : "Following";
+
+    this._isFollowed = !this._isFollowed;
   }
 }
 
@@ -123,11 +178,12 @@ for (let i = 1; i <= 15; i++) {
   const avatarURL = faker.image.avatar();
   const imageUrl = faker.image.urlLoremFlickr();
   const description = faker.lorem.word();
+  const hashtagWord = faker.lorem.word();
 
-  const post = new Post(userName, avatarURL, imageUrl, description);
+  const post = new Post(userName, avatarURL, imageUrl, description, hashtagWord);
 
-  posts.push(post);
   post.render();
+  posts.push(post);
 }
 
-console.log(posts);
+// console.log(posts);
