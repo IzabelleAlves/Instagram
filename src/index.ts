c8a7d67787a2f188fa6b99d999ed74fa3a5a8387
Izabelle Alves <izabelle.alvesbl@gmail.com>
2024-09-10 22:02:57 -0300

    Adicionados sidebar e barra de story

--- src/index.ts
+++ src/index.ts
@@ -1,12 +1,54 @@
 import { faker } from "@faker-js/faker";
 import { v4 as randomUUID } from "uuid"; // npm i --save-dev @types/uuid
 
+class Story {
+  private _avatarStory: string;
+  private _userNameStory: string;
+
+  constructor(avatarStory: string, userNameStory: string) {
+    this._avatarStory = avatarStory;
+    this._userNameStory = userNameStory;
+  }
+
+  createStoryBar() {
+    const storyBar = document.createElement("div");
+    storyBar.className = "story-bar"; // Adicione uma classe para estilização
+
+    storyBar.innerHTML = `
+      <div class="story">
+          <div class="storyImage">
+          <img
+            src="https://cdn.pixabay.com/photo/2018/11/13/22/01/instagram-3814080_640.png"
+          />
+          </div>
+      </div>
+    `;
+
+    // Adiciona a barra de stories ao contêiner da barra inicial
+
+    const barraInicial = document.getElementById("barraInicial");
+    if (barraInicial) {
+      barraInicial.appendChild(storyBar);
+    }
+  }
+}
+// Crie a barra de stories quando a página carregar
+const stories: Story[] = [];
+
+for (let i = 1; i <= 15; i++) {
+  const userNameStory = faker.person.firstName();
+  const avatarStory = faker.image.avatar();
+
+  const story = new Story(userNameStory, avatarStory);
+  story.createStoryBar();
+  stories.push(story);
+}
+
 class Post {
-  
-  private _userName: string; 
-  private _description: string; 
-  private _createdAt: Date = new Date(); 
-  private _numberOfLikes: number = 0; 
+  private _userName: string;
+  private _description: string;
+  private _createdAt: Date = new Date();
+  private _numberOfLikes: number = 0;
   private _hashtag: string;
   private _id: string = randomUUID();
   private _isLiked: boolean = false;
@@ -22,10 +64,10 @@ class Post {
     description: string,
     hashtag: string
   ) {
-    this._userName = userName; 
-    this._description = description; 
+    this._userName = userName;
+    this._description = description;
     this._avatarUrl = avatarUrl;
-    this._imageUrl = imageUrl; 
+    this._imageUrl = imageUrl;
     this._hashtag = hashtag;
   }
 
@@ -36,68 +78,72 @@ class Post {
     const postHeader = document.createElement("div");
     postHeader.className = "post-header";
     postHeader.innerHTML = `
-          <div class="avatar-user">
-              <div class="avatar">
-                <img
-                  src="${this._avatarUrl}"
-                  alt=""
-                  srcset=""
-                />
-              </div>
-              <span>${this._userName}</span>
-            </div>
-            <div id="btn-follow-${this._id}" class="follow-options">
-              <div class="follow">Follow</div>
-              <div>...</div>
-            </div>`;
+      <div class="avatar-user">
+        <div class="avatar">
+          <img
+            src="${this._avatarUrl}"
+            alt=""
+            srcset=""
+          />
+        </div>
+        <span>${this._userName}</span>
+      </div>
+      <div id="btn-follow-${this._id}" class="follow-options">
+        <div class="follow">Follow</div>
+        <div>...</div>
+      </div>
+    `;
 
     const postImage = document.createElement("div");
     postImage.className = "post-image";
     postImage.innerHTML = `<img
-              src="${this._imageUrl}"
-              alt="${this._description}"
-              srcset=""
-            />`;
+      src="${this._imageUrl}"
+      alt="${this._description}"
+      srcset=""
+    />`;
 
     const postIcons = document.createElement("div");
     postIcons.className = "post-icons";
-    postIcons.innerHTML = `<div>
-              <div id="btn-like-${this._id}">
-                <i class="fa fa-heart-o"></i>
-              </div>
-
-              <div>
-                <i class="fa fa-comment-o"></i>
-              </div>
-
-              <div>
-                <i class="fa fa-send-o"></i>
-              </div>
-            </div>
-            <div id="btn-save-${this._id}">
-              <i class="fa fa-bookmark-o"></i>
-            </div>`;
+    postIcons.innerHTML = `
+      <div>
+        <div id="btn-like-${this._id}">
+          <i class="fa fa-heart-o"></i>
+        </div>
+        <div>
+          <i class="fa fa-comment-o"></i>
+        </div>
+        <div>
+          <i class="fa fa-send-o"></i>
+        </div>
+      </div>
+      <div id="btn-save-${this._id}">
+        <i class="fa fa-bookmark-o"></i>
+      </div>
+    `;
 
     const postLike = document.createElement("div");
     postLike.className = "post-likes";
-    postLike.innerHTML = ` <i class="fa fa-heart"></i>
-            <span id="like-count-${this._id}">0</span> likes`;
+    postLike.innerHTML = `<i class="fa fa-heart"></i>
+      <span id="like-count-${this._id}">0</span> likes`;
 
     const postDescription = document.createElement("div");
     postDescription.className = "post-description";
-    postDescription.innerHTML = `<div class="user_name">username</div>
-            <div class="description">
-              ${this._description} <span class="hash-tag">#${this._hashtag}</span>
-            </div>`;
+    postDescription.innerHTML = `
+      <div class="user_name">${this._userName}</div>
+      <div class="description">
+        ${this._description} <span class="hash-tag">#${this._hashtag}</span>
+      </div>
+    `;
 
     const postComments = document.createElement("div");
     postComments.className = "post-comments";
     postComments.innerHTML = `
-            <div class="comments-list" id="comments-list-${this._id}"></div>
-              <div class="comment-input">
-                <input type="text" id="comment-input-${this._id}" placeholder="Add a comment..." />
-                <button id="comment-submit-${this._id}">Post</button>
-              </div>`;
+      <div class="comments-list" id="comments-list-${this._id}"></div>
+      <div class="comment-input">
+        <input type="text" id="comment-input-${this._id}" placeholder="Add a comment..." />
+        <button id="comment-submit-${this._id}">Post</button>
+      </div>
+    `;
 
     postContainer.append(
       postHeader,
@@ -113,8 +159,8 @@ class Post {
       mainContainer.appendChild(postContainer);
     }
 
-    const likeButton = document.querySelector(`#btn-like-${this._id}`)
-    if (likeButton){
+    const likeButton = document.querySelector(`#btn-like-${this._id}`);
+    if (likeButton) {
       likeButton.addEventListener("click", () => this.like());
     }
 
@@ -127,7 +173,7 @@ class Post {
     if (followButton) {
       followButton.addEventListener("click", () => this.follow());
     }
-    
+
     const commentButton = document.querySelector(`#comment-submit-${this._id}`);
     if (commentButton) {
       commentButton.addEventListener("click", () => this.addComment());
@@ -137,12 +183,9 @@ class Post {
   }
 
   like() {
-
     const button = document.getElementById(`btn-like-${this._id}`);
     const icon = button?.querySelector("i");
-
     let likeCountElement = document.getElementById(`like-count-${this._id}`);
-
     if (!icon || !likeCountElement) return;
 
     icon.classList.toggle("fa-heart");
@@ -151,14 +194,12 @@ class Post {
 
     this._numberOfLikes += this._isLiked ? -1 : 1;
     likeCountElement.innerText = this._numberOfLikes.toString();
-
     this._isLiked = !this._isLiked;
   }
 
   save() {
     const button = document.getElementById(`btn-save-${this._id}`);
     const icon = button?.querySelector("i");
-
     if (!icon) return;
 
     icon.classList.toggle("fa-bookmark");
@@ -171,7 +212,6 @@ class Post {
   follow() {
     const button = document.querySelector(`#btn-follow-${this._id}`);
     const icon = button?.querySelector("div");
-
     if (!icon) return;
 
     icon.classList.toggle("followed-style");
@@ -183,7 +223,6 @@ class Post {
   addComment() {
     const commentInput = document.getElementById(`comment-input-${this._id}`) as HTMLInputElement;
     const commentsList = document.getElementById(`comments-list-${this._id}`);
-
     if (!commentInput || !commentsList) return;
 
     const commentText = commentInput.value.trim();
@@ -212,7 +251,6 @@ for (let i = 1; i <= 15; i++) {
   const hashtagWord = faker.lorem.word();
 
   const post = new Post(userName, avatarURL, imageUrl, description, hashtagWord);
-
   post.render();
   posts.push(post);
 }
