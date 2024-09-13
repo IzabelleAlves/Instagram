97ae15aaf27f0203a2d15b66e52b852f7119d339
Izabelle Alves <izabelle.alvesbl@gmail.com>
2024-09-13 18:29:55 -0300

    Estruturação profile page

--- src/profilePage.ts
+++ src/profilePage.ts
@@ -1,9 +1,36 @@
 import { faker } from "@faker-js/faker";
+import { v4 as uuidv4 } from "uuid"; // npm i --save-dev @types/uuid
 
-class HomePage {
-  private _avatarUrl: string;
+class Profile {
+  private _imageUrl: string;
 
-  constructor(avatarUrl: string) {
-    this._avatarUrl = avatarUrl
+  constructor(imageUrl: string) {
+    this._imageUrl = imageUrl;
+  }
+
+  profilePhotos() {
+    const photos = document.createElement("div");
+    photos.className = "image-blocks";
+    photos.innerHTML = `<img src="${this._imageUrl}" alt="" />  
+    `;
+
+    const getPhotos = document.getElementById("container-profile");
+    if (getPhotos) {
+      getPhotos.appendChild(photos);
+    }
+    return photos;
   }
 }
+
+document.addEventListener("DOMContentLoaded", () => {
+  const postedPhotos: Profile[] = [];
+
+  for (let i = 1; i <= 15; i++) {
+    const imageUrl = faker.image.urlLoremFlickr();
+
+    const feed = new Profile(imageUrl);
+    postedPhotos.push(feed);
+
+    feed.profilePhotos();
+  }
+});
